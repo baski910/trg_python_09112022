642a7d267886ccbb9695dd6b4814fba3e2ab38de
baski910 <baskar910@gmail.com>
2022-11-09 11:18:09 +0530

    module example 3

--- 09112022/hello.py
+++ 09112022/hello.py
@@ -1,5 +1,18 @@
+#__all__ = ['addnum','divnum'] # restrict functions to be imported by from modulename import * directive
 def addnum(a,b):
     return a+b
 
 def divnum(a,b):
     return a/b
+
+#def sayhello():
+#    print("helloworld")
+
+# __name__ - returns name of the current namespace
+# __name__ will be equal to __main__ in currently executing script
+#          will be equal to namespace in calling script
+
+if __name__ == '__main__':
+    print("calling from module:",__name__) 
+    print("calling from module:",addnum(50,5))
+    print("calling from module:",divnum(50,5))
