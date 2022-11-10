23bd458b94a015d193f5df8fd454cc5ebd6a0a7a
baski910 <baskar910@gmail.com>
2022-11-10 11:40:53 +0530

    django example 2

--- 10_11_2022/simpleproject/simpleapp/views.py
+++ 10_11_2022/simpleproject/simpleapp/views.py
@@ -7,4 +7,9 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("<h1>Welcome to Django app</h1>")
-    return render(request,"simpleapp/welcome.html")
+    #msg = 'this is a test message from django application'
+    #return render(request,"simpleapp/welcome.html",{'message': msg})
+    #list_of_names =['bob','tom','pat','raj']
+    #return render(request,"simpleapp/welcome.html",{'names': list_of_names})
+    list_of_names =[{'name':'bob'},{'name':'tom'},{'name':'pat'},{'name':'raj'}]
+    return render(request,"simpleapp/welcome.html",{'names': list_of_names})
