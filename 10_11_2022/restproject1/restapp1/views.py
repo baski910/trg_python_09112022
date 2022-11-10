1cb4c35af1036675d99cf6a029f7e9a8401e13d4
baski910 <baskar910@gmail.com>
2022-11-10 16:27:40 +0530

    django rest project

---
+++ 10_11_2022/restproject1/restapp1/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializers import BookSerializers
+# Create your views here.
+
+class BookViews(APIView):
+    def get(self, request, id=None):
+        if id:
+            item = Book.objects.get(id=id)
+            serializer = BookSerializers(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+        items = Book.objects.all()
+        serializer = BookSerializers(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+    def post(self,request):
+        print(request.data)
+        serializer = BookSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status":"success","data":request.data},status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
